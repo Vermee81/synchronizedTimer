eeae89bb15b106a771e02cb9619029c484022d2d
Vermee81 <hrksb5029@gmail.com>
2018-10-20 11:23:38 +0900

    [init] Initial Xcode project

---
+++ syncronizedTimer/syncronizedTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  syncronizedTimer
+//
+//  Created by Hiroo Kusaba on 2018/10/20.
+//  Copyright © 2018年 Hiroo Kusaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
